35b8fba15d843f455899f2dd17009fd4b14d20e7
McMcMc12 <61702904+McMcMc12@users.noreply.github.com>
2023-05-10 17:58:35 +0800

    UpdatedMobileMarket

--- app/src/main/java/com/example/myapplication/ItemCartDialog.java
+++ app/src/main/java/com/example/myapplication/ItemCartDialog.java
@@ -18,34 +18,60 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
+import com.stripe.android.PaymentConfiguration;
+import com.stripe.android.paymentsheet.PaymentSheet;
+import com.stripe.android.paymentsheet.PaymentSheetResult;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 
 public class ItemCartDialog extends AppCompatDialogFragment {
+
+    PaymentSheet paymentSheet;
+    String paymentIntentClientSecret ;
+    PaymentSheet.CustomerConfiguration configuration;
+    Button buyButton;
 
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-
         LayoutInflater inflater = requireActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.item_dialog, null);
-
-
+        fetchAPI();
+        System.out.println(paymentIntentClientSecret);
         TextView name = view.findViewById(R.id.textView11);
         TextView cat = view.findViewById(R.id.textView18);
         TextView price = view.findViewById(R.id.textView19);
         TextView dis = view.findViewById(R.id.textView20);
         ImageView iv = view.findViewById(R.id.imageView8);
         Button Buy = view.findViewById(R.id.button6);
+
         Buy.setText("Buy");
         Button back = view.findViewById(R.id.button7);
         back.setText("Remove");
+
 
         Bundle bundle = getArguments();
 
@@ -64,7 +90,38 @@ public class ItemCartDialog extends AppCompatDialogFragment {
         dis.setText(ItemDis);
         Picasso.get().load(ItemUrl).fit().centerCrop().into(iv);
 
-        System.out.println(ItemKey);
+        //Java-to-PHP
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://fyp2-7b907.web.app";
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{"
+                + "\"Name\": \"" + ItemName + "\","
+                + "\"Category\": \"" + ItemCat + "\","
+                + "\"Price\": \"" + ItemPrice + "\","
+                + "\"Dis\": \"" + ItemDis + "\","
+                + "\"Url\": \"" + ItemUrl + "\","
+                + "\"Key\": \"" + ItemKey + "\""
+                + "}");
+
+        okhttp3.Request okHttpRequest = new okhttp3.Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Call call = client.newCall(okHttpRequest);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Handle error
+            }
+
+            @Override
+            public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                // Handle response
+            }
+        });
 
 
         back.setOnClickListener(v -> {
@@ -81,10 +138,77 @@ public class ItemCartDialog extends AppCompatDialogFragment {
         });
 
 
+
+        Buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (paymentIntentClientSecret != null) {
+                    paymentSheet.presentWithPaymentIntent(paymentIntentClientSecret,
+                            new PaymentSheet.Configuration("Prototype Gateway", configuration));
+                }else{
+                    Toast.makeText(getContext(), "API is loading  .  .  . "+ paymentIntentClientSecret, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        paymentSheet = new PaymentSheet(this, this::onPaymentSheetResult);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(view);
+
         return builder.create();
 
+    }
+
+
+    private  void onPaymentSheetResult(final PaymentSheetResult paymentSheetResult){
+        if(paymentSheetResult instanceof PaymentSheetResult.Canceled){
+            Toast.makeText(getContext(), "Payment has been canceled", Toast.LENGTH_SHORT).show();
+        }
+        if(paymentSheetResult instanceof PaymentSheetResult.Failed){
+            Toast.makeText(getContext(), ((PaymentSheetResult.Failed) paymentSheetResult).getError().getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        if(paymentSheetResult instanceof PaymentSheetResult.Completed){
+            Toast.makeText(getContext(), "Payment is complete", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void fetchAPI(){
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        String url ="https://fyp2-7b907.web.app";//replace with demo server
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            configuration = new PaymentSheet.CustomerConfiguration(
+                                    jsonObject.getString("customer"),
+                                    jsonObject.getString("ephemeralKey")
+                            );
+                            paymentIntentClientSecret = jsonObject.getString("paymentIntent");
+                            PaymentConfiguration.init(getActivity().getApplicationContext(), jsonObject.getString("publishableKey"));
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }){
+            protected Map<String, String> getParams(){
+                Map<String,String> paramV = new HashMap<>();
+                paramV.put("authKey", "abc");
+                return paramV;
+            }
+        };
+        queue.add(stringRequest);
     }
 
 }
